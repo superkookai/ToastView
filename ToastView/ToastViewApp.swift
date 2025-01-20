b22db353e11550d7408f91fb75948f4f78887f76
Weerawut <weerawut@live.com>
2025-01-20 09:22:07 +0700

    Initial Commit

---
+++ ToastView/ToastViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastViewApp.swift
+//  ToastView
+//
+//  Created by Weerawut Chaiyasomboon on 20/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToastViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
